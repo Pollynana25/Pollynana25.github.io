eaeda9a24557b80ccd43f9b8e98711180e1601e8
Pollynana25 <carlucci.lavoro@gmail.com>
2025-07-30 11:09:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -169,14 +169,16 @@
 
     <canvas id="trail"></canvas>
     <audio id="barkSound" src="https://www.fesliyanstudios.com/play-mp3/6548" preload="auto"></audio>
-      <script>
-  window.addEventListener("load", function () {
-    const preloader = document.getElementById("preloader");
-    if (preloader) {
-      preloader.style.display = "none";
-    }
-  });
-</script>
+
+    <script>
+      window.addEventListener("load", function () {
+        const preloader = document.getElementById("preloader");
+        if (preloader) {
+          preloader.style.display = "none";
+        }
+      });
+    </script>
+
     <script src="script.js"></script>
   </body>
 </html>
